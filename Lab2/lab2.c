cf6e257694ccad2f38630983a184561721f14446
Scott <scott.edgerly@maine.edu>
2015-02-12 18:46:20 -0500

    Part 1 of the Lab is working!

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -4,19 +4,21 @@
 int main(int argc, char *argv[]) {
 	char file_path[] = "/proc/meminfo";
 	FILE *fp;
-
-	fp = fopen(file_path,"r");
-	if (fp == NULL) {
-		perror("fopen");
-		return errno;
-	}
-
-	char buff[100];https://github.com/jeremiahsimonsen/ECE477.git
+	char buff[100];
+	char str[100];
 	while(1) {
-		buff = fgets()
+		fp = fopen(file_path,"r");
+		if (fp == NULL) {
+			perror("fopen");
+			return errno;
+		}
+		while(fgets(buff, 100, fp)!=NULL){ // scan until EoF
+			sscanf(buff, "%s", str); // extract the first word in the string
+			if(strcmp(str,"MemFree:") == 0){ // if the first word is MemFree , print the line
+				printf("%s\n",buff); // print the value of MemFree
+			}
+		}
+		fclose(fp); //close the file and restart
 	}
-
-	fclose(fp);
-
 	return 0;
 }
