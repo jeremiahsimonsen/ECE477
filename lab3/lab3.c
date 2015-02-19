ed1581f03b31efb4006e02707beb45de22043efb
jeremiahsimonsen <jeremiah.simonsen@maine.edu>
2015-02-18 21:30:19 -0500

    Simple blink program for testing the serial port and LED connection. Works.

---
+++ lab3/lab3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	int fd, serial;
+
+	fd = open("/dev/ttyUSB0",O_RDWR);
+	if (fd < 0) {
+		close(fd);
+		perror("TTY open");
+		return errno;
+	}
+	ioctl(fd,TIOCMGET, &serial);
+	while(1) {
+		serial |= TIOCM_DTR;
+		ioctl(fd,TIOCMSET, &serial);
+		sleep(1);
+		serial &= ~TIOCM_DTR;
+		ioctl(fd,TIOCMSET, &serial);
+		sleep(1);
+	}
+	
+	close(fd);
+
+	return 0;
+}
