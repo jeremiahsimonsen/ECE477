dc58bf96ca4c71d72b7ac538a9bae176a460295f
= <jeremiah.simonsen@maine.edu>
2015-02-18 23:15:47 -0500

    Started working with timer shit

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -6,23 +6,38 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#include <unistd.h>
+#include <sys/io.h>
+
+#define CONTROL 0x43
+
 int main(int argc, char *argv[])
 {
 	int fd, serial;
 
+	if (ioperm(0x40,5,1) < 0) {
+		perror("Permissions");
+		return(errno);
+	}
+
+	char byte = inb(CONTROL);
+	printf("%x\n", byte);
+
+	/* Open serial port */
 	if ( (fd = open("/dev/ttyUSB0",O_RDWR) )< 0) {
 		close(fd);
 		perror("TTY open");
 		return errno;
 	}
-	ioctl(fd,TIOCMGET, &serial);
+	ioctl(fd,TIOCMGET, &serial);		// Read the serial port
+	/* Toggle LED forever */
 	while(1) {
-		// serial |= TIOCM_DTR;
-		// ioctl(fd,TIOCMSET, &serial);
-		// sleep(1);
-		// serial &= ~TIOCM_DTR;
-		// ioctl(fd,TIOCMSET, &serial);
-		// sleep(1);
+		serial |= TIOCM_DTR;
+		ioctl(fd,TIOCMSET, &serial);
+		sleep(1);
+		serial &= ~TIOCM_DTR;
+		ioctl(fd,TIOCMSET, &serial);
+		sleep(1);
 	}
 	
 	close(fd);
