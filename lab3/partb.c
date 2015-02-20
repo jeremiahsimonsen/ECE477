daa7f977e51052fae0ba0df79c982c3e3c9d09b3
= <jeremiah.simonsen@maine.edu>
2015-02-19 20:03:18 -0500

    Added song

--- lab3/partb.c
+++ lab3/partb.c
@@ -15,6 +15,45 @@
 #define CONTROL 0x43
 #define SPEAKER 0x61
 
+#define NOTE_A 440
+#define NOTE_B 494
+#define NOTE_D 294
+#define NOTE_G 392
+
+// B A G A B B B
+// A A A B D D
+// B A G A B B B
+// B A A B A G
+// B A	G	A	B	B	B
+// A A	A	B	D	D
+// B A G A B B B
+// B A A B A G
+// D B A G A B B B
+// A A A B D D
+// D B A G A B B B
+// B A A B A G
+// D B A G A B B B
+// A A A B D D
+// D B A G A B B B
+// B A A B A G
+static const mary_lamb[108] = {NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_A, NOTE_A, NOTE_A, NOTE_B, NOTE_D, NOTE_D,
+	NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_B, NOTE_A, NOTE_A, NOTE_B, NOTE_A, NOTE_G,
+	NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_A, NOTE_A, NOTE_A, NOTE_B, NOTE_D, NOTE_D,
+	NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B,NOTE_B,
+	NOTE_B, NOTE_A, NOTE_A, NOTE_B, NOTE_A, NOTE_G,
+	NOTE_D, NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_A, NOTE_A, NOTE_A, NOTE_B, NOTE_D, NOTE_D,
+	NOTE_D, NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_B, NOTE_A, NOTE_A, NOTE_B, NOTE_A, NOTE_G,
+	NOTE_D, NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_A, NOTE_A, NOTE_A, NOTE_B, NOTE_D, NOTE_D,
+	NOTE_D, NOTE_B, NOTE_A, NOTE_G, NOTE_A, NOTE_B, NOTE_B, NOTE_B,
+	NOTE_B, NOTE_A, NOTE_A, NOTE_B, NOTE_A, NOTE_G};
+
+
 void tone_512() {
 	char byte;
 	outb(0xb6,0x43);
@@ -38,7 +77,7 @@ void beep() {
 	outb(old,0x61);
 }
 
-int note(unsigned int frequency, unsigned int delay)
+int note(unsigned int frequency, float delay)
 {
 	char new,old;
 	// if (frequency > 5000) return 1;		// can't generate high freq well
@@ -50,7 +89,7 @@ int note(unsigned int frequency, unsigned int delay)
 	old = new;
 	new |= 3;
 	outb(new,0x61);
-	sleep(delay);
+	usleep(delay*1000);
 	outb(old,0x61);
 }
 
@@ -60,10 +99,15 @@ int main(int argc, char *argv[])
 		perror("Permissions");
 		return errno;
 	}
-	
+
+	// int i;
+	// for(i=100;i<5000;i+=100) {
+	// 	note(i,1);
+	// }
+
 	int i;
-	for(i=100;i<5000;i+=100) {
-		note(i,1);
+	for (i=0; i<104; i++) {
+		note(mary_lamb[i],500);
 	}
 
 	return 0;
