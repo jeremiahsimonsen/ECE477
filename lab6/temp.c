ef0a4ebe35b09227e7b5f2d6920837df13d06a49
Scott Edgerly <scott.edgerly@maine.edu>
2015-04-11 21:25:22 +0000

    transmits a null character every second.

--- lab6/temp.c
+++ lab6/temp.c
@@ -1,15 +1,24 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
+#include <util/delay.h>
 #define F_CPU 1843200// Clock Speed
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
 
-void UART_Init( unsigned int ubrr );
-
+void UART_Init( unsigned int );
+unsigned char ADC_Start_Conv(void);
+void USART_Transmit(unsigned char);
 void main( void )
 {
-	USART_Init ( MYUBRR );
+	unsigned char data;
+	USART_Init( MYUBRR );
+	TEMP_Init();
+	while(1){
+	data = ADC_Start_Conv();
+	USART_Transmit(data);
+	_delay_ms(1000);
+	}
 }
 
 
@@ -29,7 +38,7 @@ void USART_Transmit( unsigned char data )  //may or may not need this
 /* Wait for empty transmit buffer */
 	while ( !( UCSR0A & (1<<UDRE0)) );
 /* Put data into buffer, sends the data */
-	UDR0 = data;
+	UDR0 = data;  //send out the MS bits of the ADCH
 }
 
 unsigned char USART_Receive( void )  //may or may not need this
@@ -43,14 +52,15 @@ unsigned char USART_Receive( void )  //may or may not need this
 
 void TEMP_Init( void )
 {
-	ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<MUX3);  //I belive this sets the temp sensor check page 247 for more info. Also sets the internal 1.1V reference.
+	ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<ADLAR)|(1<<MUX3);  //I belive this sets the temp sensor check page 247 for more info. Also sets the internal 1.1V reference.
 	ADCSRA = (1<<ADEN);  //enable the ADC
 }
 
-void ADC_Start_Conv(unsigned char data)
+unsigned char ADC_Start_Conv( void )
 {
+	unsigned char data;
 	ADCSRA = (1<<ADSC);  //Starts the conversion
-	while (ADCSRA & (1<<ADSC) == 1);  //Wait for the conversion to finish
-	data = (ADCH<<8)| ADCL;  //Grab the data from the two registers.
+	while ((ADCSRA & (1<<ADSC)) == 1);  //Wait for the conversion to finish
+	data = ADCH;  //Grab the data from the two registers.
 	return data;
 }
