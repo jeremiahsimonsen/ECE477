0a502257e443dd5fa4d3e3a4b802745ad1e24567
Scott Edgerly <scott.edgerly@maine.edu>
2015-04-11 15:07:06 +0000

    added Transmit and Recieve functions.

--- lab6/temp.c
+++ lab6/temp.c
@@ -23,5 +23,23 @@ UBRR0L = (unsigned char)ubrr;
 /* Enable receiver and transmitter */
 UCSR0B = (1<<4)|(1<<3); //4 = RXEN 3 = TXEN
 /* Set frame format: 8data, 2stop bit */
-UCSR0C = (1<<7)|(1<<3)|(3<<1);
+UCSR0C = (1<<7)|(1<<3)|(3<<1);  //(1<<URSEL)|(1<<USBS)|(3<<UCSZ0);
+}
+
+void USART_Transmit( unsigned char data )  //may or may not need this
+{
+/* Wait for empty transmit buffer */
+while ( !( UCSR0A & (1<<UDRE0)) )
+;
+/* Put data into buffer, sends the data */
+UDR0 = data;
+}
+
+unsigned char USART_Receive( void )  //may or may not need this
+{
+/* Wait for data to be received */
+while ( !(UCSR0A & (1<<RXC0)) )
+;
+/* Get and return received data from buffer */
+return UDR0;
 }
