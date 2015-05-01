30b569a247a4751fdcbbfe4d2e1b578ee1b711ac
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-05-01 12:30:58 -0400

    Updated

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -4,27 +4,40 @@
 #include <avr/boot.h>
 
 void main(void) {
-	uint8_t sreg;
-	int i,j;
+	uint16_t buf;
+	int i, address = 0;
 
-	sreg = SREG;
+	// Disable interrupts
 	cli();
+
+	eeprom_busy_wait();
+
+	// Erase the page
+	boot_page_erase_safe(address);
+	// Wait until the memory is erased
 	boot_spm_busy_wait();
 
-	for (j = 0; j < 4; j++) {
-		boot_page_erase_safe(j<<6);
-		boot_spm_busy_wait();
+	// Enable the RWW-section
+	boot_rww_enable();
+	while(boot_rww_busy());		// Wait until it's enabled
 
-		for (i = 0; i < 64; i+=2) {
-			boot_page_fill_safe(i, (j)|(i<<7));
+	// For the first 4 pages...
+	for(address = 0; address < 4*SPM_PAGESIZE; address += SPM_PAGESIZE) {
+
+		// For each word (2B) in the current page
+		for (i = 0; i < SPM_PAGESIZE; i += 2) {
+			// Set up little-endian word
+			buf = (i/2)<<8;		// high byte
+			buf |= (((i/2) + address/SPM_PAGESIZE) / 32));	// low byte
 		}
+	}
 
-		boot_page_write_safe(j<<6);
-		boot_spm_busy_wait();
-		boot_rww_enable();
-	}	
+	// Enable the RWW-section
+	boot_rww_enable();
 
-	SREG = sreg;
+	// Enable interrupts
+	sei();
+
 	while(1);
 
 	return 0;
