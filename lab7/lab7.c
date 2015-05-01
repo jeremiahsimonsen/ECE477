119e9d0459334e6136689f7d2fe77a40a2266300
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-05-01 09:50:35 -0400

    tx

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,32 +1,31 @@
+#include <inttypes.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <avr/boot.h>
 
-int main(void) {
-	int addr = 0x0000;
-	char low = 0x00;
-	char high = 0x00;
-	// int i = 0;
+void main(void) {
+	uint8_t sreg;
+	int i,j;
 
-	// Verify using avrdude -t
-	// dump flash 0 128
+	sreg = SREG;
+	cli();
+	boot_spm_busy_wait();
 
-	// Erase the first 4 pages of memory
-	// 1 word = 2 Bytes
-	// 1 page = 32 words = 64 bytes
-	boot_page_erase_safe(0x0);
-	// boot_page_erase_safe(0x40);
-	// boot_page_erase_safe(0x80);
-	// boot_page_erase_safe(0x100);
+	for (j = 0; j < 4; j++) {
+		boot_page_erase_safe(j<<6);
+		boot_spm_busy_wait();
 
-	// Fill the page buffer
-	for (addr = 0x0000; addr <= 0x0040; addr +=2) {
-		// boot_page_fill	(address,data);
-		boot_page_fill(addr, ((high<<8)|low));
-		low++;
-	}
-	// Write the page buffer
-	boot_page_write_safe(0x0000);
+		for (i = 0; i < 64; i+=2) {
+			boot_page_fill_safe(i, (j)|(i<<7));
+		}
 
-// boot_page_write_safe	(address)	
+		boot_page_write_safe(j<<6);
+		boot_spm_busy_wait();
+		boot_rww_enable();
+	}	
+
+	SREG = sreg;
+	while(1);
 
 	return 0;
 }
